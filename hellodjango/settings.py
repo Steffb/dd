fc51d7bb883a4fb145ef9df6c5e0bce88fe4586f
Steffb <steffenf.birkeland@gmail.com>
2014-09-02 13:19:42 +0200

    removed unused db

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -58,10 +58,7 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 
 DATABASES = {
 
-'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+
     
 }
 
